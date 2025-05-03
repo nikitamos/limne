48b0610e65db2cf35e5d3c814ca4f61522cb6108
Nikita Moskaliov <nikwit10@gmail.com>
2025-05-03 12:55:00 +0300

    Add serial bitonic sort implementation

--- src/solvers/bitonic-sorter.wgsl
+++ src/solvers/bitonic-sorter.wgsl
@@ -1,10 +1,32 @@
-var<workgroup> a: f32;
+const WG_SIZE: u32 = 1024u;
+struct Particle {
+  density: f32,
+  pos: vec3<f32>,
+  velocity: vec3<f32>,
+  forces: vec3<f32>,
+}
+struct Params {
+  offset: u32,
+  h: u32,
+}
 
-@compute @workgroup_size(1024)
-fn flip_1024() {}
+var<workgroup> a: array<Particle, WG_SIZE>;
+var<push_constant> p: Params;
 
-@compute @workgroup_size(1024)
-fn disperse_1024() {}
+fn do_flip(offset: u32, pair: u32) {
 
-@compute @workgroup_size(1)
-fn global_layer() {}
+}
+
+@compute @workgroup_size(WG_SIZE)
+fn flip_local() {
+  
+}
+
+@compute @workgroup_size(WG_SIZE)
+fn disperse_local() {}
+
+@compute @workgroup_size(WG_SIZE)
+fn flip_global() {}
+
+@compute @workgroup_size(WG_SIZE)
+fn disperse_global() {}
--- src/solvers/bitonic_sorter.rs
+++ src/solvers/bitonic_sorter.rs
@@ -1,6 +1,14 @@
-use wgpu::ComputePipelineDescriptor;
+use wgpu::{
+  ComputePassDescriptor, ComputePipelineDescriptor, PipelineCompilationOptions,
+  PipelineLayoutDescriptor, PushConstantRange, ShaderModuleDescriptor, ShaderStages,
+};
+pub const LOCAL_PASS_SIZE: usize = 1024;
 
+#[cfg(test)]
 mod test {
+  use std::clone;
+
+  use rand::{distr::Distribution, rngs::StdRng, SeedableRng};
   use wgpu::{InstanceDescriptor, RequestAdapterOptions};
 
   async fn setup_wgpu() -> Option<(wgpu::Device, wgpu::Queue)> {
@@ -23,41 +31,145 @@ mod test {
 
   #[tokio::test]
   async fn test_1() {}
-}
 
-fn sort_256_cpu(a: &[i32]) {
-  assert_eq!(a.len(), 256);
-  for i in 0..8 { // sort passes & distances
-    // swaps that can be done in parallel
-    for j in 0..128 {
-      
+  fn cas(a: &mut [i32], l: usize, r: usize) {
+    if a[l] > a[r] {
+      a.swap(l, r);
     }
-    // Barrier here?
+  }
+
+  fn cpu_serial_bitonic_sort(a: &mut [i32]) {
+    assert_eq!(a.len().count_ones(), 1, "Array length must be a power of 2");
+    if a.len() == 1 {
+      return;
+    }
+    let n = a.len();
+    let k = n.trailing_zeros() as usize;
+    for t in (0..=(k - 1)).rev() {
+      // 2^t is count of flip blocks
+      // height of a flip block?
+      let flh = 1 << (k - t);
+      // iterate over flip blocks
+      for flb in 0..(1 << t) {
+        let go = flh * flb;
+        for lo in 0..flh / 2 {
+          cas(a, go + lo, go + flh - lo - 1);
+        }
+      }
+      // <BARRIER>
+      // 'stages'
+      for q in (1..=(k - t)).rev() {
+        // 2^q is the height of disperse block in the stage
+        let dbc = 1 << (k - q);
+        let dbh = 1 << q;
+        for i in 0..dbc {
+          let go = i * dbh;
+          for j in 0..dbh / 2 {
+            cas(a, go + j, go + j + dbh / 2);
+          }
+        }
+        // <BARRIER>
+      }
+    }
+  }
+
+  #[test]
+  fn cpu_bitonic_sort_4() {
+    let mut a = [2, 3, 1, 4];
+    let mut b = a.clone();
+    cpu_serial_bitonic_sort(&mut b);
+    a.sort();
+    assert_eq!(a, b);
+  }
+
+  #[test]
+  fn cpu_bitonic_sort_64() {
+    let mut a = array::<64>(-20, 20);
+    let mut b = a.clone();
+    a.sort();
+    cpu_serial_bitonic_sort(&mut b);
+    assert_eq!(a, b);
+  }
+
+  fn array<const N: usize>(min: i32, max: i32) -> [i32; N] {
+    let mut rng = rand::rng();
+    let d = rand::distr::Uniform::new_inclusive(min, max).unwrap();
+    let mut a = [0; N];
+    for i in a.iter_mut() {
+      *i = d.sample(&mut rng);
+    }
+    a
+  }
+
+  #[test]
+  fn cpu_bitonic_sort_1024x1024() {
+    for _ in 0..1024 {
+      let mut a = array::<1024>(-1000, 1000);
+      let mut b = a.clone();
+      a.sort();
+      cpu_serial_bitonic_sort(&mut b);
+      assert_eq!(a, b);
+    }
   }
 }
 
-pub fn bitonic_sort(a: &[i32]) {
-  assert_eq!(a.len().count_ones(), 1, "Length is not power of 2");
-  assert!(a.len() >= 256, "Length is less than 256");
+pub struct ParticleBitonicSorter {
+  flip_local: wgpu::ComputePipeline,
+  disperse_local: wgpu::ComputePipeline,
 }
 
-pub struct BitonicSorter {
-  pipeline: wgpu::ComputePipeline,
-}
-
-impl BitonicSorter {
-  pub fn new(device: &wgpu::Device, queue: &wgpu::Queue) -> BitonicSorter {
-    let pipeline = device.create_compute_pipeline(&ComputePipelineDescriptor {
-      label: todo!(),
-      layout: todo!(),
-      module: todo!(),
-      entry_point: todo!(),
-      compilation_options: todo!(),
-      cache: todo!(),
+impl ParticleBitonicSorter {
+  pub fn new(
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+    particle_layout: &wgpu::BindGroupLayout,
+  ) -> ParticleBitonicSorter {
+    let module = device.create_shader_module(wgpu::include_wgsl!("bitonic-sorter.wgsl"));
+    let module = &module;
+    let layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+      label: Some("BitonicSorter layout"),
+      bind_group_layouts: &[particle_layout],
+      push_constant_ranges: &[PushConstantRange {
+        stages: wgpu::ShaderStages::COMPUTE,
+        range: 0..3,
+      }],
     });
-    todo!()
+    let layout = Some(&layout);
+    let flip_local = device.create_compute_pipeline(&ComputePipelineDescriptor {
+      label: Some("BitonicSorter::flip_local"),
+      layout,
+      module,
+      entry_point: Some("flip_local"),
+      compilation_options: Default::default(),
+      cache: None,
+    });
+    let disperse_local = device.create_compute_pipeline(&ComputePipelineDescriptor {
+      label: Some("BitonicSorter::disperse_local"),
+      layout,
+      module,
+      entry_point: Some("disperse_local"),
+      compilation_options: Default::default(),
+      cache: None,
+    });
+    ParticleBitonicSorter {
+      flip_local,
+      disperse_local,
+    }
   }
   fn sort_256(&self) {
     todo!()
+  }
+  pub fn set_buffer(&self, device: &wgpu::Device, buffer: &wgpu::Buffer) {
+    todo!()
+  }
+  pub fn sort(&self, encoder: &mut wgpu::CommandEncoder, particles: &wgpu::BindGroup) {
+    {
+      let mut pass = encoder.begin_compute_pass(&ComputePassDescriptor {
+        label: Some("flip local CP"),
+        timestamp_writes: None,
+      });
+      pass.set_push_constants(0, &0u32.to_ne_bytes());
+      // pass.dis
+    }
   }
 }
