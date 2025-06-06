fd8a6588e497fbd84d0d5b2c3e6fbc7e9f7139b3
Nikita Moskaliov <nikwit10@gmail.com>
2025-06-06 12:50:02 +0300

    Exclude amnis

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    cc::Build::new()
-        .std("c11")
-        .file("amnis/renderdoc.c")
-        .compile("amnis-rdoc");
-    println!("cargo::rerun-if-changed=amnis/renderdoc.c");
-    // eprintln!()
-}
